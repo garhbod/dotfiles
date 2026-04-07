201ad98e024b2238a389fe010466c4b8dde16797
Ryan Worth <ryan.design@live.com>
2026-04-08 00:49:46 +1000

    Update shell and terminal theming

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+# --- Nix environment (works everywhere) ---
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end
+
+# Optional: user profile (sometimes needed depending on install)
+if test -e $HOME/.nix-profile/etc/profile.d/nix.fish
+    source $HOME/.nix-profile/etc/profile.d/nix.fish
+end
+
+# --- Interactive-only stuff ---
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # prompt, aliases, etc.
+    
+    # Initialize Starship
+    starship init fish | source
+end
